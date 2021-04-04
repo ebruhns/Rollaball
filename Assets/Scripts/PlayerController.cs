617d20d7dbab69c303ef5046e5eef649bf07df28
ebruhns <elliebruhns@gmail.com>
2021-04-03 23:50:43 -0700

    jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -21,44 +21,49 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-	count = 0;
+	    count = 0;
 
-	SetCountText();
-	winTextObject.SetActive(false);
+	    SetCountText();
+	    winTextObject.SetActive(false);
     }
-
+    
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
         
-   	movementX = movementVector.x;
-	movementY = movementVector.y;
+   	    movementX = movementVector.x;
+	    movementY = movementVector.y;
+    }
+
+    void OnJump()
+    {
+        
     }
 
     void SetCountText()
     {
-	countText.text = "Count: " + count.ToString();
-	if (count >= 12)
-	{
-	    winTextObject.SetActive(true);
-	}
+	    countText.text = "Count: " + count.ToString();
+	    if (count >= 12)
+	    {
+	        winTextObject.SetActive(true);
+	   }
     }
 
     void FixedUpdate()
     {
-	Vector3 movement = new Vector3(movementX, 0.0f, movementY);	
-
+	    Vector3 movement = new Vector3(movementX, 0.0f, movementY);	
         rb.AddForce(movement * speed);
+        
     }
 
     private void OnTriggerEnter(Collider other)
     {
-	if(other.gameObject.CompareTag("PickUp"))
-	{
+	    if(other.gameObject.CompareTag("PickUp"))
+	    {
     	    other.gameObject.SetActive(false);
-	    count++;
-	    SetCountText();
-	}
+	        count++;
+	        SetCountText();
+	    }
     }
 
 }
